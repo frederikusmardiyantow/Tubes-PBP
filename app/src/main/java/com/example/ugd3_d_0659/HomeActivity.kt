aac1e8967c635bbc13ae243b417ffc2391eeb979
johanYW <johanwahyu49@gmail.com>
2022-09-06 10:51:23 +0700

    1

--- app/src/main/java/com/example/ugd3_d_0659/HomeActivity.kt
+++ app/src/main/java/com/example/ugd3_d_0659/HomeActivity.kt
@@ -2,6 +2,12 @@ package com.example.ugd3_d_0659
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.DialogInterface
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
