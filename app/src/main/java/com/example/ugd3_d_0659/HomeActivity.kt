ddd3a137e0c6dd4e01634d58b0097f116ebcef1e
frederikusmardiyantow <frederikus.mardiyanto@gmail.com>
2022-09-06 09:19:33 +0700

    comitCoba

---
+++ app/src/main/java/com/example/ugd3_d_0659/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ugd3_d_0659
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
