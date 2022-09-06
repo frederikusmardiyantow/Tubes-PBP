5cbf522bfb0cedff5135468c731e89c04f8c60ab
frederikusmardiyantow <frederikus.mardiyanto@gmail.com>
2022-09-06 08:06:44 +0700

    UGD3 Commit

---
+++ app/src/main/java/com/example/ugd3_d_0659/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ugd3_d_0659
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
