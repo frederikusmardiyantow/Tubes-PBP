c9ffb29b9ebadda753f5bf86c1cc5e5f6e7ef822
Edmond <edmondsoren@gmail.com>
2022-09-20 23:23:46 +0700

    tambah view binding

--- app/src/main/java/com/example/ugd3_d_0659/RegistrationActivity.kt
+++ app/src/main/java/com/example/ugd3_d_0659/RegistrationActivity.kt
@@ -2,12 +2,12 @@ package com.example.ugd3_d_0659
 
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.ugd3_d_0659.databinding.ActivityRegistrationBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
@@ -22,28 +22,31 @@ class RegistrationActivity : AppCompatActivity() {
     private lateinit var inputTelepon : TextInputLayout
     private lateinit var inputTextTLLangsung : TextInputEditText
     private lateinit var regisLayout: ConstraintLayout
-
+    var binding: ActivityRegistrationBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        //setContentView(R.layout.activity_registration)
+
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
 
         val cal = Calendar.getInstance()
         val myYear = cal.get(Calendar.YEAR)
         val myDay = cal.get(Calendar.DAY_OF_MONTH)
         val myMonth = cal.get(Calendar.MONTH)
 
-        getSupportActionBar()?.hide()
-
-        inputNama = findViewById(R.id.inputLayoutNama)
-        inputUsernameRegis = findViewById(R.id.inputLayoutUsernameRegis)
-        inputEmail =findViewById(R.id.inputLayoutEmail)
-        inputPasswordRegis = findViewById(R.id.inputLayoutPasswordRegis)
-        inputConfirm = findViewById(R.id.inputLayoutKonfirmPassword)
-        inputTL = findViewById(R.id.inputLayoutTanggalLahir)
-        inputTelepon = findViewById(R.id.inputLayoutTelepon)
-        regisLayout =findViewById(R.id.regisLayout)
-        inputTextTLLangsung = findViewById(R.id.inputTextTanggalLahirLangsung)
+        supportActionBar?.hide()
+        //inputNama = findViewById(R.id.inputLayoutNama)
+        //inputUsernameRegis = findViewById(R.id.inputLayoutUsernameRegis)
+        //inputEmail =findViewById(R.id.inputLayoutEmail)
+        //inputPasswordRegis = findViewById(R.id.inputLayoutPasswordRegis)
+        //inputConfirm = findViewById(R.id.inputLayoutKonfirmPassword)
+        //inputTL = findViewById(R.id.inputLayoutTanggalLahir)
+        //inputTelepon = findViewById(R.id.inputLayoutTelepon)
+        //regisLayout =findViewById(R.id.regisLayout)
+        //inputTextTLLangsung = findViewById(R.id.inputTextTanggalLahirLangsung)
 
 
         var btnBack: Button = findViewById(R.id.btnBack)
@@ -56,59 +59,59 @@ class RegistrationActivity : AppCompatActivity() {
 
         btnRegis.setOnClickListener(View.OnClickListener {
             var checkRegis = false
-            val nama : String = inputNama.getEditText()?.getText().toString()
-            val username: String = inputUsernameRegis.getEditText()?.getText().toString()
-            val email : String = inputEmail.getEditText()?.getText().toString()
-            val password: String = inputPasswordRegis.getEditText()?.getText().toString()
-            val confirm : String = inputConfirm.getEditText()?.getText().toString()
-            val tanggal : String = inputTL.getEditText()?.getText().toString()
-            val telepon : String = inputTelepon.getEditText()?.getText().toString()
+            val nama : String = binding!!.inputLayoutNama.editText?.text.toString()
+            val username: String = binding!!.inputLayoutUsernameRegis.editText?.text.toString()
+            val email : String = binding!!.inputLayoutEmail.editText?.text.toString()
+            val password: String = binding!!.inputLayoutPasswordRegis.editText?.text.toString()
+            val confirm : String = binding!!.inputLayoutKonfirmPassword.editText?.text.toString()
+            val tanggal : String = binding!!.inputLayoutTanggalLahir.editText?.text.toString()
+            val telepon : String = binding!!.inputLayoutTelepon.editText?.text.toString()
             val mBundle = Bundle()
 
             if(nama.isEmpty()){
-                inputNama.setError("Nama tidak boleh kosong")
+                inputNama.error = "Nama tidak boleh kosong"
                 checkRegis = false
             }
 
             if(username.isEmpty()){
-                inputUsernameRegis.setError("Username tidak boleh kosong")
+                inputUsernameRegis.error = "Username tidak boleh kosong"
                 checkRegis = false
             }
 
             if(email.isEmpty()){
-                inputEmail.setError("Email tidak boleh kosong")
+                inputEmail.error = "Email tidak boleh kosong"
                 checkRegis = false
             }
 
             if(password.isEmpty()){
-                inputPasswordRegis.setError("Password tidak boleh kosong")
+                inputPasswordRegis.error = "Password tidak boleh kosong"
                 checkRegis = false
             }
 
             if(confirm.isEmpty()){
-                inputConfirm.setError("Password tidak boleh kosong")
+                inputConfirm.error = "Password tidak boleh kosong"
                 checkRegis = false
             }
 
             if(password.length!=8 || confirm.length!=8){
-                inputPasswordRegis.setError("Password harus 8 karakter!")
-                inputConfirm.setError("Password harus 8 karakter!")
+                inputPasswordRegis.error = "Password harus 8 karakter!"
+                inputConfirm.error = "Password harus 8 karakter!"
                 checkRegis = false
             }
 
             if(confirm!=password){
-                inputPasswordRegis.setError("Password tidak sama!")
-                inputConfirm.setError("Password tidak sama!")
+                inputPasswordRegis.error = "Password tidak sama!"
+                inputConfirm.error = "Password tidak sama!"
                 checkRegis = false
             }
 
             if(tanggal.isEmpty()){
-                inputTL.setError("Tanggal lahir tidak boleh kosong")
+                inputTL.error = "Tanggal lahir tidak boleh kosong"
                 checkRegis = false
             }
 
             if(telepon.isEmpty()){
-                inputTelepon.setError("Nomor telepon tidak boleh kosong")
+                inputTelepon.error = "Nomor telepon tidak boleh kosong"
                 checkRegis = false
             }
 
@@ -128,7 +131,7 @@ class RegistrationActivity : AppCompatActivity() {
 
         inputTextTLLangsung.setOnFocusChangeListener { view, b ->
             val datePicker= DatePickerDialog(this,DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
-                inputTextTLLangsung.setText("${dayOfMonth}/${(month.toInt()+1).toString()}/${year}")
+                inputTextTLLangsung.setText("${dayOfMonth}/${(month.toInt()+1)}/${year}")
 
             }, myYear, myMonth, myDay)
 
