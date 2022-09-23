bb2fe1ed60abe5fe2589f30131c210c309cc837d
Edmond <edmondsoren@gmail.com>
2022-09-23 18:11:34 +0700

    tambah note

---
+++ app/src/main/java/com/example/ugd3_d_0659/room/Note.kt
@@ -0,0 +1,12 @@
+package com.example.ugd3_d_0659.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
