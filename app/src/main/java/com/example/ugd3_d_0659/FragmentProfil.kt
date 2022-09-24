9e9d467dc0bc8bffab9c99db3b79db374bd45093
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2022-09-24 09:35:32 +0700

    Perbaikan Profil + Manifest

--- app/src/main/java/com/example/ugd3_d_0659/FragmentProfil.kt
+++ app/src/main/java/com/example/ugd3_d_0659/FragmentProfil.kt
@@ -1,33 +1,30 @@
 package com.example.ugd3_d_0659
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.ugd3_d_0659.room.User
+import com.example.ugd3_d_0659.room.UserDB
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.fragment_profil.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentProfil.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentProfil : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val myPreference = "myPref"
+    private val id = "nameKey"
+    var sharedPreferences: SharedPreferences? = null
+    private lateinit var nama : TextView
+    private lateinit var username : TextView
+    private lateinit var email : TextView
+    private lateinit var password : TextView
+    private lateinit var tglLahir : TextView
+    private lateinit var noTelp : TextView
+    private lateinit var user : User
+    val db by lazy { activity?.let { UserDB(it) } }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +34,33 @@ class FragmentProfil : Fragment() {
         return inflater.inflate(R.layout.fragment_profil, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentProfil.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentProfil().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = activity?.getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+
+        val id = sharedPreferences!!.getInt(id,0)
+
+        nama = view.findViewById(R.id.namaDiProfil)
+        username = view.findViewById(R.id.usernameDiProfil)
+        email = view.findViewById(R.id.emailDiProfil)
+        password = view.findViewById(R.id.passwordDiProfil)
+        tglLahir = view.findViewById(R.id.tanggalDiProfil)
+        noTelp = view.findViewById(R.id.telpDiProfil)
+
+        user = db?.userDao()?.getUser(id)?.get(0)!!
+
+        nama.text = user.nama
+        username.text = user.username
+        email.text = user.email
+        password.text = user.password
+        tglLahir.text = user.tglLahir
+        noTelp.text = user.noTelp
+
+        btnEditDiProfil.setOnClickListener(View.OnClickListener{
+            var mFragmentEditProfil = FragmentEditProfil()
+
+            (activity as HomeActivity).changeFragment(mFragmentEditProfil)
+        })
     }
+
 }
