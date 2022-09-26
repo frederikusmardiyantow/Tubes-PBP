6ae83fe326c6fd3c908e9933a77d447479b38710
Edmond <edmondsoren@gmail.com>
2022-09-26 21:00:20 +0700

    benerin click notif supaya pindah ke login

--- app/src/main/java/com/example/ugd3_d_0659/RegistrationActivity.kt
+++ app/src/main/java/com/example/ugd3_d_0659/RegistrationActivity.kt
@@ -174,6 +174,9 @@ class RegistrationActivity : AppCompatActivity() {
     }
 
     private fun sendNotification() {
+        val intent: Intent = Intent(this,MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
         val pendingIntent: PendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_MUTABLE)
         val broadcastIntent: Intent = Intent(this, NotificationReceiver::class.java)
         broadcastIntent.putExtra("toastMessage", binding.inputLayoutUsernameRegis.editText?.text.toString())
