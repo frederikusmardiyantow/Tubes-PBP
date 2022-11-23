034ed0badf0c36be7990236fcf0dcbc21a210776
Edmond <edmondsoren@gmail.com>
2022-11-23 10:32:07 +0700

    add library openPDF

--- app/src/main/java/com/example/ugd3_d_0659/webAPI/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/ugd3_d_0659/webAPI/adapter/NoteAdapter.kt
@@ -1,14 +1,15 @@
 package com.example.ugd3_d_0659.webAPI.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ugd3_d_0659.EditNoteActivity
@@ -16,8 +17,26 @@ import com.example.ugd3_d_0659.MenuNote
 import com.example.ugd3_d_0659.R
 import com.example.ugd3_d_0659.webAPI.models.Note
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.ArrayList
+import com.itextpdf.barcodes.BarcodeQRCode
+import com.itextpdf.kernel.colors.ColorConstants
+import com.itextpdf.kernel.geom.PageSize
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Cell
+import com.itextpdf.layout.element.Image
+import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Table
+import com.itextpdf.layout.property.HorizontalAlignment
+import com.itextpdf.layout.property.TextAlignment
 
 class NoteAdapter(private  var noteList: List<Note>, context: Context) :
     RecyclerView.Adapter<NoteAdapter.ViewHolder>(), Filterable {
@@ -70,6 +89,70 @@ class NoteAdapter(private  var noteList: List<Note>, context: Context) :
             if(context is MenuNote)
                 context.startActivityForResult(i, MenuNote.LAUNCH_ADD_ACTIVITY)
         }
+        holder.btnPrint.setOnClickListener {
+            val title= holder.tvTitle.text.toString()
+            val note = holder.tvNote.text.toString()
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    createPdf(title,note)
+                }
+            }catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }
+        }
+    }
+    @SuppressLint("ObsoleteSdkInt")
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Throws(
+        FileNotFoundException::class
+    )
+    private fun createPdf(title: String, note: String) {
+        val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+        val file = File(pdfPath, "pdf_note.pdf")
+        FileOutputStream(file)
+        val writer = PdfWriter(file)
+        val pdfDocument = PdfDocument(writer)
+        val document = Document(pdfDocument)
+
+        pdfDocument.defaultPageSize = PageSize.A4
+        document.setMargins(5f, 5f, 5f, 5f)
+        val namapengguna = Paragraph("Note").setBold().setFontSize(24f)
+            .setTextAlignment(TextAlignment.CENTER)
+        val group = Paragraph(
+            """
+                                Note yang di Print : 
+                            """.trimIndent()).setTextAlignment(TextAlignment.CENTER).setFontSize(12f)
+        val width = floatArrayOf(100f,100f)
+        val table = Table(width)
+
+        table.setHorizontalAlignment(HorizontalAlignment.CENTER)
+        table.addCell(Cell().add(Paragraph("Title")))
+        table.addCell(Cell().add(Paragraph(title)))
+        table.addCell(Cell().add(Paragraph("Note")))
+        table.addCell(Cell().add(Paragraph(note)))
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        table.addCell(Cell().add(Paragraph("Tanggal buat PDF")))
+        table.addCell(Cell().add(Paragraph(LocalDate.now().format(dateTimeFormatter))))
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss a")
+        table.addCell(Cell().add(Paragraph("Pukul Pembuatan")))
+        table.addCell(Cell().add(Paragraph(LocalTime.now().format(timeFormatter))))
+        val barcodeQRCode = BarcodeQRCode(
+            """
+                $nama
+                $umur
+                ${LocalDate.now().format(dateTimeFormatter)}
+                ${LocalTime.now().format(timeFormatter)}
+            """.trimIndent())
+        val qrCodeObject = barcodeQRCode.createFormXObject(ColorConstants.BLACK, pdfDocument)
+        val qrCodeImage = Image(qrCodeObject).setWidth(80f).setHorizontalAlignment(HorizontalAlignment.CENTER)
+
+        document.add(namapengguna)
+        document.add(group)
+        document.add(table)
+        document.add(qrCodeImage)
+
+        document.close()
+        Toast.makeText(this, "Pdf Created", Toast.LENGTH_LONG).show()
     }
 
     override fun getFilter(): Filter {
@@ -104,12 +187,14 @@ class NoteAdapter(private  var noteList: List<Note>, context: Context) :
         var tvNote: TextView
         var btnDelete: ImageButton
         var cvNote: CardView
+        var btnPrint: ImageButton
 
         init {
             tvTitle= itemView.findViewById(R.id.tv_title_note)
             tvNote=itemView.findViewById(R.id.tv_note)
             btnDelete=itemView.findViewById(R.id.btn_delete)
             cvNote=itemView.findViewById(R.id.cv_note)
+            btnPrint=itemView.findViewById(R.id.btn_print)
         }
     }
 }
