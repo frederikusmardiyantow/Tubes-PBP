01b4008d0b84af33d27aaafb9b571ebc30af7157
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2022-12-01 20:50:30 +0700

    Testing EditBukuActivity -clear

---
+++ app/src/androidTest/java/com/example/ugd3_d_0659/EditBukuActivityTest.kt
@@ -0,0 +1,285 @@
+package com.example.ugd3_d_0659
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class EditBukuActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(EditBukuActivity::class.java)
+
+    @Test
+    fun editBukuActivityTest() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.et_judul_buku),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_judul_buku),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("Kancil"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton2.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.et_penerbit_buku),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_penerbit_buku),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(replaceText("Erlangga"), closeSoftKeyboard())
+
+        val materialButton3 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton3.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText3 = onView(
+            allOf(
+                withId(R.id.et_tahunTebit_buku),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_tahunTerbit_buku),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText3.perform(replaceText("2019"), closeSoftKeyboard())
+
+        val materialButton4 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton4.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText4 = onView(
+            allOf(
+                withId(R.id.et_pengarang_buku),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_pengarang_buku),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText4.perform(replaceText("Santosa"), closeSoftKeyboard())
+
+        val materialButton5 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton5.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText5 = onView(
+            allOf(
+                withId(R.id.et_jumlahHalaman_buku),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_jumlahHalaman_buku),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText5.perform(replaceText("50"), closeSoftKeyboard())
+
+        val materialButton6 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton6.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText6 = onView(
+            allOf(
+                withId(R.id.et_isbn_buku),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_isbn_buku),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText6.perform(replaceText("1234567889912"), closeSoftKeyboard())
+
+        val materialButton7 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout3),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            8
+                        )
+                    ),
+                    1
+                )
+            )
+        )
+        materialButton7.perform(scrollTo(), click())
+        Espresso.onView(isRoot()).perform(waitFor(3000))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    fun waitFor(delay: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for " + delay + "milliseconds"
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+                uiController!!.loopMainThreadForAtLeast(delay)
+            }
+        }
+    }
+}
